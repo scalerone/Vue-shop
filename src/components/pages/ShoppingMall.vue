aba97d28d57432e73997c5217bf6232bf2638fed
scalerone <676119236@qq.com>
2019-11-09 23:55:54 +0800

    init project

---
+++ src/components/pages/ShoppingMall.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{msg}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                msg: "shopping mall"
+            }
+        },
+    }
+</script>
+
+<style  scoped>
+
+</style>
